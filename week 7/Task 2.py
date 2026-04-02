bcd0df0c6d240a88aed1adf931f06cdcded6592a
latiftariq755-netizen <latiftariq755@gmail.com>
2026-04-02 23:04:37 +0500

    Add files via upload
    This is my 2nd task

---
+++ week 7/Task 2.py
@@ -0,0 +1,27 @@
+obt_marks = int(input("Enter your marks: "))
+t_marks = int(input("Enter your total marks: "))
+
+percentage = (obt_marks/t_marks)*100
+print("Your percentage is:", percentage)
+
+if percentage >= 90:
+    print("Grade: A")   
+elif percentage >= 85:
+    print("Grade: A-")   
+elif percentage >= 80:
+    print("Grade: B+")
+elif percentage >= 75:
+    print("Grade: B")
+elif percentage >= 70:
+    print("Grade: B-")
+elif percentage >= 65:
+    print("Grade: C+")
+elif percentage >= 60:
+    print("Grade: C")
+elif percentage >= 55:
+    print("Grade: C-")
+elif percentage >= 50:
+    print("Grade: D")
+
+else:
+    print("Grade: F")
