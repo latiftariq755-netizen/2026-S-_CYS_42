eb5563efb2b7b996a3b23a3a63c10ae1b64e87b9
latiftariq755-netizen <latiftariq755@gmail.com>
2026-04-04 01:47:37 +0500

    Add files via upload
    Task 4: Basic calculations

---
+++ week 7/task 4.py
@@ -0,0 +1,39 @@
+# Task 1 : Add two numbers and print the result.
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+c = a%b
+print("The result is:", c)
+
+# Task 2 : Two no.s as input and print its rremainder
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+c = a%b
+print("The result is:", c)
+
+# Task 3 : Two no.s as input and print its division
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+c = a/b
+print("The result is:", c)
+
+# Task 4 : Two no.s as input and print the result of a%b + a/b
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+c = a%b+a/b
+print("The result is:", c)
+ 
+ # Task 5: check lists.
+a = [1,2,3]
+print(a[2])
+
+# Task 6: Print name from list.
+a = [1,"Latif",3]
+print(a[1])
+
+# Task 7: Print error by accessing index which is not present in list.
+a = (1,"Latif",3)
+print(a[4])
